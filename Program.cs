418f587fe4507fa96f42a0c3543b6d63098f876b
BinhTran <fam.out16@gmail.com>
2024-08-12 21:44:15 +0700

    Add cac files cua bai thuc hanh kiem tra so nguyen to

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SUDUNGCAULENHLAP
+{
+    class KiemTraSoNguyenTo
+    {
+        static void Main(string[] args)
+        {
+            int socankiemtra;
+            Console.WriteLine("Nhap vao so can xet:");
+            socankiemtra = int.Parse(Console.ReadLine());
+
+            if (socankiemtra>=2)
+            {
+                if (socankiemtra<=3)
+                {
+                    Console.WriteLine("{0} la so nguyen to", socankiemtra);
+                }
+                int sochia = 2;
+                while(sochia <= Math.Sqrt(socankiemtra))
+                {
+                    if (socankiemtra%sochia==0)
+                    {
+                        Console.WriteLine("{0} khong phai so nguyen to", socankiemtra);
+                        break;
+                    }
+                    else Console.WriteLine("{0} la so nguyen to", socankiemtra);
+                    sochia++;
+                }
+            } else Console.WriteLine("{0} khong phai so nguyen to", socankiemtra);
+        }
+    }
+}
